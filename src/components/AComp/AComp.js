f78f3e5536f5f3a15f465e3b319d7e73765209d3
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2019-11-11 15:29:30 +0000

    test prettier

--- src/components/AComp/AComp.js
+++ src/components/AComp/AComp.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 import './acomp.scss';
 
-export const AComp = ({ children, one, two, three }) => (
+export const AComp = ({
+                        children,
+                        one,
+                        two,
+                        three
+  }) => (
   <div className="acomp">
     <h2 className="acomp__title">Test Component</h2>
     <div className="acomp__content">{children}</div>
