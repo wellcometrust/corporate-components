25e8e80f1ee88785ea91b1d94854126d9a65f118
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2019-11-12 16:11:35 +0000

    #5601 feat(chore): Linting - Removed extraneous dependency pre-commit as it is superceded by husky

--- src/components/AComp/AComp.js
+++ src/components/AComp/AComp.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 import './acomp.scss';
 
-export const AComp = ({
-                        children,
-                        one,
-                        two,
-                        three
-  }) => (
+export const AComp = ({ children, one, two, three }) => (
   <div className="acomp">
     <h2 className="acomp__title">Test Component</h2>
     <div className="acomp__content">{children}</div>
