5c5a81cd1d7924b2ea2d2bb7cc91ea8210cc8a90
Vlad Zhigulsky <vlad@d3digital.co.uk>
2019-11-13 17:25:34 +0000

    install postcss plugins

--- rollup.config.js
+++ rollup.config.js
@@ -6,10 +6,12 @@ import babel from 'rollup-plugin-babel';
 import url from 'rollup-plugin-url';
 import autoprefixer from 'autoprefixer';
 import postcss from 'rollup-plugin-postcss';
+import postcssCustomProperties from 'postcss-custom-properties';
+import postcssCalc from 'postcss-calc';
 
 const globals = {
-  'react': 'React',
-  'react-dom': 'ReactDOM',
+  react: 'React',
+  'react-dom': 'ReactDOM'
 };
 
 export default {
@@ -31,7 +33,7 @@ export default {
         // left-hand side can be an absolute path, a path
         // relative to the current directory, or the name
         // of a module in node_modules
-        'node_modules/lodash.throttle/index.js': [ 'throttle' ],
+        'node_modules/lodash.throttle/index.js': ['throttle'],
         'node_modules/react-is/index.js': [
           'isElement',
           'isValidElementType',
@@ -44,13 +46,13 @@ export default {
       include: ['**/*.woff', '**/*.woff2'],
       // setting infinite limit will ensure that the files
       // are always bundled with the code, not copied to /dist
-      limit: Infinity,
+      limit: Infinity
     }),
     json(),
     postcss({
-      extract: 'dist/style.css',
+      extract: 'dist/styles.css',
       minimize: true,
-      plugins: [autoprefixer()]
+      plugins: [autoprefixer(), postcssCustomProperties(), postcssCalc()]
     })
-  ],
-}
+  ]
+};
