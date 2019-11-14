fe592d5f0c57736183751ea66a410694070c2246
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2019-11-14 17:25:00 +0000

    #5590 feat: typescript - fixed broken glob pattern matching

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  collectCoverageFrom: ['<rootDir>/src/**/*.(js|ts)x?'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx,ts,tsx}'],
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/src/test/mocks/file.mock.js',
@@ -7,8 +7,8 @@ module.exports = {
   },
   setupFilesAfterEnv: ['<rootDir>/config/setupTests.js'],
   testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.(js|ts)x?',
-    '<rootDir>/src/**/?(*.)(test).(js|ts)x?'
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(test).{js,jsx,ts,tsx}'
   ],
   transformIgnorePatterns: [
     '/node_modules/'
--- src/components/AComp/AComp.tsx
+++ src/components/AComp/AComp.tsx
@@ -4,11 +4,8 @@ type ACompProps = {
   children?: any
 }
 
-export const AComp = ({ children }: ACompProps) => (
-  <div className="acomp">
-    <h2 className="acomp__title">Test Component</h2>
-    <div className="acomp__content">{children}</div>
-  </div>
-);
+export const AComp = ({children}: ACompProps) =>(
+<div className="acomp"><h2 className="acomp__title">Test Component</h2><div className="acomp__content">{children}</div></div>
+)
 
 export default AComp;
