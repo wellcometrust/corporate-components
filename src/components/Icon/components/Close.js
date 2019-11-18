98fd8f5e2bf599c41030238d58814f9111882941
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2019-11-18 11:08:41 +0000

    #5590 feat: TypeScript - Button and Icon component migration (WIP)
    - added SVG icons to support spearhead for adding TS to all components

---
+++ src/components/Icon/components/Close.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SvgCross = props => (
+  <svg viewBox="0 0 19 18" {...props}>
+    <g fill="currentColor" fillRule="evenodd">
+      <path d="M17.632.161l.707.707-16.97 16.97-.708-.706z" />
+      <path d="M1.368.161L.661.868l16.97 16.97.708-.706z" />
+    </g>
+  </svg>
+);
+
+export default SvgCross;
---
+++ src/components/Icon/components/Download.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SvgDownload = props => (
+  <svg viewBox="0 0 21 16" {...props}>
+    <g fill="currentColor" fillRule="evenodd">
+      <path d="M19.5 7.75v6.75h-18V7.75H0V15a1 1 0 0 0 1 1h19a1 1 0 0 0 1-1V7.75h-1.5z" />
+      <path d="M9.75 0v9.775L5.782 5.807 4.72 6.868l5.78 5.78 5.779-5.78-1.062-1.06-3.968 3.968V0z" />
+    </g>
+  </svg>
+);
+
+export default SvgDownload;
