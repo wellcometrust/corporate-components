72f2fa2c97438f92eb59f72f0305cce70b7bbc2b
chriddell <chrisliddell1991@gmail.com>
2019-11-20 14:48:58 +0000

    #5607 feat(refactor): tweak Sass structure to allow for _base import to component styles

--- src/components/AComp/AComp.js
+++ src/components/AComp/AComp.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 export const AComp = ({ children }) => (
-  <div className="c-acomp">
-    <h2 className="c-acomp__title">Test Component</h2>
-    <div className="c-acomp__content">{children}</div>
+  <div className="acomp">
+    <h2 className="acomp__title">Test Component</h2>
+    <div className="acomp__content">{children}</div>
   </div>
 );
 
