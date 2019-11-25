50f8d981112d9870fc5ec9a043813e8b502cb513
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2019-11-25 17:20:29 +0000

    #5180 feat: Header components
    - updated storybook to compile component styles and allow global settings
    - added Icon component styles

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -4,6 +4,9 @@ import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 import { addReadme } from 'storybook-readme';
 
+// import SASS styles globally
+import '!style-loader!css-loader!sass-loader!./scss-loader.scss';
+
 function loadStories() {
   const req = require.context('../../src', true, /\.stories\.(js|ts)x?$/);
 
