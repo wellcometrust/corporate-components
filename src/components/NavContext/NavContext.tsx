f833c90ff73c3a226b7341632a55dbc787a7c6e9
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-01-02 11:44:11 +0000

    #5846 feat: added NavContext component

---
+++ src/components/NavContext/NavContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from 'react';
+
+const defaultState = {
+  isNavActive: false,
+  openNav: () => {}
+};
+
+type NavContextProps = {
+  isNavActive: boolean;
+  openNav: (bool: boolean) => void;
+};
+
+type ProviderProps = {
+  children: React.ReactNode;
+};
+
+export const NavContext = createContext<NavContextProps>(defaultState);
+
+export const NavContextProvider = ({ children }: ProviderProps) => {
+  const [state, setState] = useState<NavContextProps>({
+    isNavActive: false,
+    openNav: bool =>
+      setState(prevState => ({ ...prevState, isNavActive: bool }))
+  });
+
+  return <NavContext.Provider value={state}>{children}</NavContext.Provider>;
+};
+
+export default NavContext;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import 'assets/styles/app.scss';
 
 export { Button } from 'Button/Button';
 export { Icon } from 'Icon/Icon';
+export { NavContext, NavContextProvider } from 'NavContext/NavContext';
 export {
   ViewportContext,
   ViewportContextProvider
