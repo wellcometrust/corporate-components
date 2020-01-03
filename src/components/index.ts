d322026d03204c90485f04071579a483d8f08520
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-01-03 10:15:35 +0000

    #5722 refactor: renamed Link to RouterLinkWrapper to better describe its job

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,19 @@
 import 'assets/styles/app.scss';
 
+// standard components
 export { Button } from 'Button/Button';
 export { Icon } from 'Icon/Icon';
-export { Link } from 'Link/Link';
+export { RouterLinkWrapper } from 'RouterLinkWrapper/RouterLinkWrapper';
+export { WellcomeCollectionBanner } from 'WellcomeCollectionBanner/WellcomeCollectionBanner';
+
+// context components
 export { NavContext, NavContextProvider } from 'NavContext/NavContext';
 export {
   SearchContext,
   SearchContextProvider
 } from 'SearchContext/SearchContext';
+
 export {
   ViewportContext,
   ViewportContextProvider
 } from 'ViewportContext/ViewportContext';
-export { WellcomeCollectionBanner } from 'WellcomeCollectionBanner/WellcomeCollectionBanner';
