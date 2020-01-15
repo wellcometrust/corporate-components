30684792ca3958fe33d8f7bc3c85bf9764e81dd8
chriddell <chrisliddell1991@gmail.com>
2020-01-15 18:02:42 +0000

    #5460 feat: adds SearchForm component

---
+++ src/components/SearchForm/SearchFormLabel.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import cx from 'classnames';
+
+type SearchFormLabelProps = {
+  children: React.ReactNode;
+  className: string;
+  textClassName: string;
+  hideText: boolean;
+};
+
+export const SearchFormLabel = ({
+  children,
+  className,
+  hideText,
+  textClassName
+}: SearchFormLabelProps) => {
+  const classNames = {
+    label: cx({
+      'search-form__label': !className,
+      [`${className}`]: className
+    }),
+    text: cx({
+      'u-visually-hidden': hideText,
+      'search-form__label-text': !textClassName,
+      [`${textClassName}`]: textClassName
+    })
+  };
+
+  return (
+    <label className={classNames.label} htmlFor="search">
+      <span className={classNames.text}>Search</span>
+      {children}
+    </label>
+  );
+};
+
+export default SearchFormLabel;
