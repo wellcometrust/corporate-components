626edd51fa21a142d6c766a831321e10da38470a
drs2034 <d.shrestha34@gmail.com>
2020-01-17 16:40:03 +0000

    #5949 - feat: extra meta information added.

--- src/components/ResultItem/ResultItem.stories.tsx
+++ src/components/ResultItem/ResultItem.stories.tsx
@@ -5,7 +5,11 @@ import { text } from '@storybook/addon-knobs';
 import ResultItem from './ResultItem';
 
 const ResultListExample = () => {
-  const meta = text('meta text', 'Funding scheme');
+  const meta = {
+    type: text('meta type', 'Funding scheme'),
+    date: text('meta date', '8 July 2016'),
+    lastUpdated: text('meta last updated', '1 November 2018')
+  };
   const title = text('title', 'Senior Research Fellowships');
   const href = text('href', '');
   const content = text(
@@ -13,7 +17,7 @@ const ResultListExample = () => {
     'Supporting independent researchers who are emerging as global leaders in their field and want to tackle the most important questions in science.'
   );
 
-  return <ResultItem meta={meta} title={title} href={href} text={content} />;
+  return <ResultItem title={title} href={href} text={content} />;
 };
 
 const stories = storiesOf('Components|ResultItem', module);
--- src/components/ResultItem/ResultItem.tsx
+++ src/components/ResultItem/ResultItem.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 
+type meta = {
+  type?: string;
+  date?: Date;
+  lastUpdated?: Date;
+};
+
 type ResultItemProps = {
   className?: string;
   href: string;
-  meta?: string;
+  meta?: meta;
   text?: string;
   title: string;
 };
 
-export const ResultItem = ({ meta, title, href, text }: ResultItemProps) => (
+export const ResultItem = ({
+  meta: { type, date, lastUpdated },
+  title,
+  href,
+  text
+}: ResultItemProps) => (
   <div className="result-item">
     <div className="result-item__meta">
-      <span className="result-item__meta-type">{meta}</span>
+      <span className="result-item__meta-type">{type}</span>
+      <span className="result-item__meta-type--date">{date}</span>
+      <span className="result-item__meta-type--last-updated">
+        Update {lastUpdated}
+      </span>
     </div>
     <h4 className="result-item__title">
       <a href={href} className="result-item__link">
