084c222b2c94fcff63efb4a64748ff80aff943f2
chriddell <chrisliddell1991@gmail.com>
2020-01-22 09:52:38 +0000

    #5972 feat(refactor): tidy ups

--- src/components/ResultItem/ResultItem.stories.tsx
+++ src/components/ResultItem/ResultItem.stories.tsx
@@ -8,23 +8,23 @@ const ResultItemExample = () => {
   const metaGroupID = 'Meta';
   const fileMetaGroupID = 'File Meta';
 
-  const title = text('Title', 'Senior Research Fellowships');
+  const title = text('title', 'Senior Research Fellowships');
   const content = text(
-    'Text',
+    'text',
     'Supporting independent researchers who are emerging as global leaders in their field and want to tackle the most important questions in science.'
   );
   const href = text('href', '#');
   const type = select('type', [null, 'file'], null);
 
   const meta = {
-    type: text('Type', 'Funding scheme', metaGroupID),
-    date: text('Date', '8 July 2016', metaGroupID),
-    lastUpdated: text('Last Updated', '10 July 2016', metaGroupID)
+    type: text('type', 'Funding scheme', metaGroupID),
+    date: text('date', '8 July 2016', metaGroupID),
+    lastUpdated: text('lastUpdated', '10 July 2016', metaGroupID)
   };
 
   const fileMeta = {
-    type: select('Type', ['PDF', 'DOC', 'MP3'], 'PDF', fileMetaGroupID),
-    size: text('Size', '257kb', fileMetaGroupID)
+    type: select('type', ['PDF', 'DOC', 'MP3'], 'PDF', fileMetaGroupID),
+    size: text('size', '257kb', fileMetaGroupID)
   };
 
   return (
