8f294be06a3a7cd3273bbeda9f3dad616972b8be
chriddell <chrisliddell1991@gmail.com>
2020-01-22 17:02:08 +0000

    #5850 feat: adds Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Footer = () => (
+  <footer className="footer">
+    <div className="footer__container">
+      <h3 className="footer__mission-statement">
+        Wellcome exists to improve health by helping great ideas to thrive
+      </h3>
+      {/* @todo add FooterNav component */}
+      {/* @todo get FooterSocial component */}
+      {/* @todo add FooterLinks component */}
+    </div>
+  </footer>
+);
+
+export default Footer;
