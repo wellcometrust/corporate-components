a4d9629c4126f071cedb42237fb9f22d2e7c0530
drs2034 <d.shrestha34@gmail.com>
2020-01-23 10:01:50 +0000

    #5850 - feat: footer links story.

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -3,7 +3,67 @@ import { storiesOf } from '@storybook/react';
 
 import Footer from './Footer';
 
-const FooterExample = () => <Footer />;
+const footerLinks = [
+  {
+    text: 'WT Grant Tracker',
+    href: 'https://wtgrants.wellcome.ac.uk/Login.aspx?ReturnUrl=%2f',
+    target: '_blank',
+    rel: 'noopener noreferrer'
+  },
+  {
+    text: 'Jobs',
+    href: '/jobs'
+  },
+  {
+    text: 'Modern slavery statement',
+    href: '/about-us/governance/modern-slavery-statement'
+  },
+  {
+    text: 'Privacy and terms',
+    href: '/about-us/governance/privacy-and-terms'
+  },
+  {
+    text: 'Accessibility',
+    href: '/about-us/governance/website-accessibility'
+  },
+  {
+    text: 'Contact Us',
+    href: '/contact-us'
+  },
+  {
+    text: 'Back to top',
+    href: '#href',
+    role: 'presentation',
+    tabIndex: -1
+  }
+];
+
+const socialLinks = [
+  {
+    text: 'Twitter',
+    className: 'footer-social__icon--twitter',
+    href: 'https://twitter.com/wellcometrust'
+  },
+  {
+    text: 'Facebook',
+    className: 'footer-social__icon--facebook',
+    href: 'https://www.facebook.com/WellcomeTrust/'
+  },
+  {
+    text: 'YouTube',
+    className: 'footer-social__icon--youtube',
+    href: 'https://www.youtube.com/user/wellcometrust'
+  },
+  {
+    text: 'Facebook',
+    className: 'footer-social__icon--linkedin',
+    href: 'https://www.linkedin.com/company/wellcome-trust'
+  }
+];
+
+const FooterExample = () => (
+  <Footer links={footerLinks} socialLinks={socialLinks} />
+);
 
 const stories = storiesOf('Components|Footer', module);
 
