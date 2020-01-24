4a679b9e25a6fd507a26d2c8fb94a6af53655864
chriddell <chrisliddell1991@gmail.com>
2020-01-24 11:58:33 +0000

    #6000 feat(refactor): tidy ups

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,19 +10,17 @@ type FooterProps = {
   socialLinks?: [];
 };
 
-export const Footer = ({ links, navigation, socialLinks }: FooterProps) => {
-  return (
-    <footer className="footer">
-      <div className="footer__container">
-        <h3 className="footer__mission-statement">
-          Wellcome exists to improve health by helping great ideas to thrive
-        </h3>
-        {navigation && <FooterNav sections={navigation} />}
-        {socialLinks && <FooterSocial links={socialLinks} />}
-        {links && <FooterLinks links={links} />}
-      </div>
-    </footer>
-  );
-};
+export const Footer = ({ links, navigation, socialLinks }: FooterProps) => (
+  <footer className="footer">
+    <div className="footer__container">
+      <h3 className="footer__mission-statement">
+        Wellcome exists to improve health by helping great ideas to thrive
+      </h3>
+      {navigation && <FooterNav sections={navigation} />}
+      {socialLinks && <FooterSocial links={socialLinks} />}
+      {links && <FooterLinks links={links} />}
+    </div>
+  </footer>
+);
 
 export default Footer;
--- src/components/Footer/FooterNav/FooterNavItem.tsx
+++ src/components/Footer/FooterNav/FooterNavItem.tsx
@@ -16,26 +16,24 @@ export const FooterNavItem = ({
   heading,
   links,
   children
-}: FooterNavItemProps) => {
-  return (
-    <li>
-      {heading.href ? (
-        <a href={heading.href}>{heading.text}</a>
-      ) : (
-        `${heading.text}`
-      )}
-      {links && (
-        <ul>
-          {links.map(({ text, href }) => (
-            <li>
-              <a href={href}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      )}
-      {children}
-    </li>
-  );
-};
+}: FooterNavItemProps) => (
+  <li>
+    {heading.href ? (
+      <a href={heading.href}>{heading.text}</a>
+    ) : (
+      `${heading.text}`
+    )}
+    {links && (
+      <ul>
+        {links.map(({ text, href }) => (
+          <li>
+            <a href={href}>{text}</a>
+          </li>
+        ))}
+      </ul>
+    )}
+    {children}
+  </li>
+);
 
 export default FooterNavItem;
