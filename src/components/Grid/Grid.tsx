db08f842c667d5d67b8c46dd99658f0fe6339e23
chriddell <chrisliddell1991@gmail.com>
2020-02-14 13:56:16 +0000

    #6117 feat(refactor): refactors GridCell component

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 
-import GridCell from './Cell/GridCell';
+import GridCell from './GridCell';
 
 type GridProps = {
   children: React.ReactNode;
