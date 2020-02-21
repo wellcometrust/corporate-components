f6b6f5973a5f354ebf9cfc0ea7796052795b548f
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-02-21 16:08:52 +0000

    #6168 feat(refactor): Cookie message - prep for moving cookie logic out of library into app

--- src/components/WellcomeCollectionBanner/WellcomeCollectionBanner.test.tsx
+++ src/components/WellcomeCollectionBanner/WellcomeCollectionBanner.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { boolean } from '@storybook/addon-knobs';
+
 import WellcomeCollectionBanner from './WellcomeCollectionBanner';
 
 describe('<WellcomeCollectionBanner />', () => {
-  const output = shallow(<WellcomeCollectionBanner />);
+  const output = shallow(<WellcomeCollectionBanner isActive />);
 
   it('renders the component', () => {
     expect(output);
--- src/components/WellcomeCollectionBanner/WellcomeCollectionBanner.tsx
+++ src/components/WellcomeCollectionBanner/WellcomeCollectionBanner.tsx
@@ -1,50 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import cx from 'classnames';
-import { CookiesProvider, useCookies } from 'react-cookie';
 
 import Icon from 'Icon/Icon';
 
-/**
- * List of page paths on which the banner will appear
- *
- * @const {string[]}
- */
-const bannerPaths = [
-  '/',
-  '/how-we-work',
-  '/about-us/contact-us',
-  '/about-us/history-wellcome'
-];
+type WellcomeCollectionBannerProps = {
+  handleItemClick?: () => void;
+  isActive: boolean;
+};
 
-export const WellcomeCollectionLink = () => {
-  const IS_WELLCOME_COLLECTION_LINK_SEEN = 'isWellcomeCollectionLinkSeen';
-  const [cookies, setCookie] = useCookies([IS_WELLCOME_COLLECTION_LINK_SEEN]);
-  const [isClient, setIsClient] = useState(false);
-
-  const handleItemClick = () => {
-    setCookie(IS_WELLCOME_COLLECTION_LINK_SEEN, 'true', {
-      path: '/',
-      maxAge: 31536000
-    });
-  };
-
-  const canShowCookieNotice = () =>
-    isClient &&
-    // cookies.cookieSeen === 'true' && // this live site cookie indicates user has consented to cookies
-    cookies[IS_WELLCOME_COLLECTION_LINK_SEEN] !== 'true' &&
-    bannerPaths.indexOf(window.location.pathname) > -1;
-
+export const WellcomeCollectionBanner = ({
+  handleItemClick,
+  isActive
+}: WellcomeCollectionBannerProps) => {
   const bannerClassName = cx('wc-banner', {
-    'is-active': canShowCookieNotice()
+    'is-active': isActive
   });
 
-  const bannerTabIndex = canShowCookieNotice() ? 0 : -1;
-
-  useEffect(() => {
-    setIsClient(true);
-
-    return () => {};
-  }, []);
+  const bannerTabIndex = isActive ? 0 : -1;
 
   return (
     <div className={bannerClassName}>
@@ -69,14 +41,6 @@ export const WellcomeCollectionLink = () => {
         </button>
       </div>
     </div>
-  );
-};
-
-export const WellcomeCollectionBanner = () => {
-  return (
-    <CookiesProvider>
-      <WellcomeCollectionLink />
-    </CookiesProvider>
   );
 };
 
