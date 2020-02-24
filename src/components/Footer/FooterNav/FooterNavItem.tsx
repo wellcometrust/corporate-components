5632682cb0904f8b614ec352601b42934546e530
chriddell <chrisliddell1991@gmail.com>
2020-02-24 14:36:46 +0000

    #6041 feat(style): adds link component

--- src/components/Footer/FooterNav/FooterNavItem.tsx
+++ src/components/Footer/FooterNav/FooterNavItem.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Link from 'Link';
+
 export type FooterNavItemProps = {
   heading: {
     text: string;
@@ -19,12 +21,12 @@ export const FooterNavItem = ({
 }: FooterNavItemProps) => (
   <li className="footer-nav__grid-item footer-nav__menu">
     {heading.href ? (
-      <a
+      <Link
         className="footer-nav__menu-title footer-nav__menu-title--link"
-        href={heading.href}
+        to={heading.href}
       >
         {heading.text}
-      </a>
+      </Link>
     ) : (
       <span className="footer-nav__menu-title">{heading.text}</span>
     )}
@@ -35,9 +37,9 @@ export const FooterNavItem = ({
             key={`footer-menu-item-${href}`}
             className="footer-nav__menu-list-item"
           >
-            <a className="footer-nav__menu-list-link" href={href}>
+            <Link to={href} className="footer-nav__menu-list-link">
               {text}
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
