619330fce634c97a52516206ba7e57b3cf405de0
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-04-29 10:15:31 +0100

    #6479 feat: Video - removed extraneous code

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -9,30 +9,17 @@ type VideoProps = {
   src: string;
 };
 
-export const Video = ({ caption, credit, className, src }: VideoProps) => {
-  const [loaded, setLoaded] = useState(false);
-
-  const onLoad = () => {
-    setLoaded(true);
-  };
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const onError = (e: any) => {
-    console.error('Video error', e);
-  };
-
-  return (
-    <Media caption={caption} className={className} credit={credit} type="video">
-      <div className="widescreen-container">
-        <iframe
-          allowFullScreen
-          className="widescreen-media"
-          src={`${src}?wmode=opaque&modestbranding=1&rel=0&showinfo=0&color=white&autohide=1`}
-          title={caption}
-        />
-      </div>
-    </Media>
-  );
-};
+export const Video = ({ caption, credit, className, src }: VideoProps) => (
+  <Media caption={caption} className={className} credit={credit} type="video">
+    <div className="widescreen-container">
+      <iframe
+        allowFullScreen
+        className="widescreen-media"
+        src={`${src}?wmode=opaque&modestbranding=1&rel=0&showinfo=0&color=white&autohide=1`}
+        title={caption}
+      />
+    </div>
+  </Media>
+);
 
 export default Video;
