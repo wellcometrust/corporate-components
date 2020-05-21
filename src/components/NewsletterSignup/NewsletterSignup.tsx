d5931cf154cc0599e42d73e9be438bb040e86fa4
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-05-21 17:14:12 +0100

    #6617 feat: Newsletter signup - added editable default title and intro copy

--- src/components/NewsletterSignup/NewsletterSignup.tsx
+++ src/components/NewsletterSignup/NewsletterSignup.tsx
@@ -3,20 +3,26 @@ import React from 'react';
 import Grid, { GridCell } from 'Grid';
 import NewsletterForm from 'NewsletterForm';
 import Section from 'Section';
-import SectionTitle from 'SectionTitle';
 
-export const NewsletterSignup = () => (
+type NewsletterSignupProps = {
+  intro: string;
+  title: string;
+};
+
+const introDefault =
+  'Get the latest news about Wellcome and the work we fund in a monthly email.';
+const titleDefault = 'Sign up to our research newsletter';
+
+export const NewsletterSignup = ({
+  intro = introDefault,
+  title = titleDefault
+}: NewsletterSignupProps) => (
   <Section className="newsletter-signup">
     <Grid>
       <GridCell column={1} columnCount={1}>
-        <h3 className="newsletter__title">
-          Sign up to our research newsletter
-        </h3>
+        <h3 className="newsletter-signup__title">{title}</h3>
         <NewsletterForm className="newsletter__form">
-          <p className="newsletter__intro">
-            Get the latest news about Wellcome and the work we fund in a monthly
-            email.
-          </p>
+          <p className="newsletter-signup__intro">{intro}</p>
         </NewsletterForm>
       </GridCell>
     </Grid>
