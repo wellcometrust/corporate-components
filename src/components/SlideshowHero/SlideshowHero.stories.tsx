fbc9a2b17ed66b38ab80353c0b807ef42cecd454
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-05-22 13:31:16 +0100

    fix: SlideshowHero storybook - added check for presence of images and added editable props

--- src/components/SlideshowHero/SlideshowHero.stories.tsx
+++ src/components/SlideshowHero/SlideshowHero.stories.tsx
@@ -1,17 +1,69 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { select, text } from '@storybook/addon-knobs';
+import { number, text } from '@storybook/addon-knobs';
 
 import SlideshowHero from './SlideshowHero';
 
 const SlideshowHeroExample = () => {
+  const animationDuration = number('animationDuration', 2000);
   const className = text('classname', '');
-  const standfirst = text('standfirst', '');
-  const title = text('title', 'Search Results');
+  const moreLink = text('moreLink', '/');
+  const skipLink = text('skipLink', '#main-content');
+  const skipLinkText = text('skipLinkText', 'Skip to main content');
+  const slides = [
+    {
+      caption: text('slide 1 caption', 'Caption for slide 1'),
+      credit: text('slide 1 credit', 'Image © my image 1')
+    },
+    {
+      caption: text('slide 2 caption', 'Caption for slide 2'),
+      credit: text('slide 2 credit', 'Image © my image 2')
+    }
+  ];
+  const standfirst = text('standfirst', 'Standfirst');
+  const title = text('title', 'Hero title');
+
+  const images = slides.map((slide, index) => ({
+    caption: slide.caption,
+    credit: slide.credit,
+    fallbackImage: 'https://placehold.it/1000x1000&text=Slide+1+medium+image',
+    id: `img${index}`,
+    imageSources: [
+      {
+        sourceType: 'image/jpeg',
+        sourceMedia: '(max-width: 767px)',
+        sourcePreload: `https://placehold.it/75x40&text=Slide+${index +
+          1}+mobile+image`,
+        sourceFull: `https://placehold.it/768x402&text=Slide+${index +
+          1}+mobile+image`
+      },
+      {
+        sourceType: 'image/jpeg',
+        sourceMedia: '(min-width: 768px) and (max-aspect-ratio: 16/9)',
+        sourcePreload: `https://placehold.it/100x100&text=Slide+${index +
+          1}+medium+image`,
+        sourceFull: `https://placehold.it/1000x1000&text=Slide+${index +
+          1}+medium+image`
+      },
+      {
+        sourceType: 'image/jpeg',
+        sourceMedia: '(min-aspect-ratio: 16/9)',
+        sourcePreload: `https://placehold.it/120x80&text=Slide+${index +
+          1}+wide+image`,
+        sourceFull: `https://placehold.it/1200x800&text=Slide+${index +
+          1}+wide+image`
+      }
+    ]
+  }));
 
   return (
     <SlideshowHero
+      animationDuration={animationDuration}
       className={className}
+      images={images}
+      moreLink={moreLink}
+      skipLink={skipLink}
+      skipLinkText={skipLinkText}
       standfirst={standfirst}
       title={title}
     />
