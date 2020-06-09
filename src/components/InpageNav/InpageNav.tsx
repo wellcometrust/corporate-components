ee6850f3801a71773940df6bd43245e05869794b
drs2034 <d.shrestha34@gmail.com>
2020-06-09 15:49:02 +0100

    #6681 - in page navigation component.

---
+++ src/components/InpageNav/InpageNav.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type InpageNavItemLinksProps = {
+  links?: InpageNavItemProps[];
+};
+
+type InpageNavItemProps = {
+  title?: string;
+  id?: string;
+};
+
+export const InpageNav = ({ links }: InpageNavItemLinksProps) => {
+  return (
+    <>
+      <h3 className="section-links__heading">On this page</h3>
+      <ul className="list-unstyled list-highlight">
+        {links.map(({ title, id, ...props }) => (
+          <li className="list-highlight__item">
+            <p className="list-highlight__title">
+              <a href={id}>{title}</a>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default InpageNav;
--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export { Header } from 'Header/Header';
 export { Icon } from 'Icon/Icon';
 export { Image } from 'Image/Image';
 export { InfoBox } from 'InfoBox/InfoBox';
+export { InpageNav } from 'InpageNav/InpageNav';
 export { LinkList } from 'LinkList/LinkList';
 export { Listing } from 'Listing/Listing';
 export { Logo } from 'Logo/Logo';
