9ae5a93933dec6e546043182c4f9da9db02e3a28
drs2034 <d.shrestha34@gmail.com>
2020-06-09 17:10:22 +0100

    #6681 - update based on feedback..

--- src/components/InpageNav/InpageNav.tsx
+++ src/components/InpageNav/InpageNav.tsx
@@ -9,21 +9,19 @@ type InpageNavItemProps = {
   id?: string;
 };
 
-export const InpageNav = ({ links }: InpageNavItemLinksProps) => {
-  return (
-    <>
-      <h3 className="section-links__heading">On this page</h3>
-      <ul className="list-unstyled list-highlight">
-        {links.map(({ title, id, ...props }) => (
-          <li className="list-highlight__item">
-            <p className="list-highlight__title">
-              <a href={id}>{title}</a>
-            </p>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+export const InpageNav = ({ links }: InpageNavItemLinksProps) => (
+  <>
+    <h3 className="section-links__heading">On this page</h3>
+    <ul className="list-unstyled list-highlight">
+      {links.map(({ title, id, ...props }) => (
+        <li className="list-highlight__item">
+          <p className="list-highlight__title">
+            <a href={id}>{title}</a>
+          </p>
+        </li>
+      ))}
+    </ul>
+  </>
+);
 
 export default InpageNav;
