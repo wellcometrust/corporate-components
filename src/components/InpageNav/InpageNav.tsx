ae9426b401bb2cb3534aed362f7597cf6951f7f5
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-06-17 13:05:04 +0100

    #6797 feat(style): InpageNav styling

--- src/components/InpageNav/InpageNav.tsx
+++ src/components/InpageNav/InpageNav.tsx
@@ -10,16 +10,18 @@ type InpageNavItemProps = {
 };
 
 export const InpageNav = ({ links }: InpageNavItemLinksProps) => (
-  <>
-    <strong className="section-links__heading">On this page</strong>
-    <ul className="list-unstyled list-highlight">
+  <div className="cc-inpage-nav">
+    <strong className="cc-inpage-nav__title">On this page</strong>
+    <ul className="cc-inpage-nav__list">
       {links.map(({ title, id, ...props }) => (
-        <li key={`anchor-link-${id}`} className="list-highlight__item">
-          <a href={`#${id}`}>{title}</a>
+        <li key={`anchor-link-${id}`} className="cc-inpage-nav__item">
+          <a className="cc-inpage-nav__link" href={`#${id}`}>
+            {title}
+          </a>
         </li>
       ))}
     </ul>
-  </>
+  </div>
 );
 
 export default InpageNav;
