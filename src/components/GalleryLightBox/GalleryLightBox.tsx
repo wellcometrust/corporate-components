4c2686e64d7018d24f66445bc14195d66f2e7536
chriddell <chrisliddell1991@gmail.com>
2020-06-18 13:03:03 +0100

    #6674 feature: adds GalleryLightBox info pane

--- src/components/GalleryLightBox/GalleryLightBox.tsx
+++ src/components/GalleryLightBox/GalleryLightBox.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import cx from 'classnames';
 
 import {
   CarouselProvider,
@@ -10,8 +11,12 @@ import {
 
 import ImageElement from 'Image/ImageElement';
 import Icon from 'Icon';
+import Button from 'Button';
+
+import ViewportContext from 'ViewportContext/ViewportContext';
 
 type GalleryLightBoxProps = {
+  isOpen?: boolean;
   slides: GalleryLightBoxSlideProps[];
 };
 
@@ -34,102 +39,143 @@ const GalleryLightBoxNav = ({
   currentSlide
 }: GalleryLightBoxNavProps) => (
   <div className="cc-gallery-lightbox__nav">
-    <ButtonBack className="cc-gallery-lightbox__nav-item cc-gallery-lightbox__nav-item--back">
+    <ButtonBack className="cc-gallery-lightbox__nav-item cc-gallery-lightbox__nav-item--back u-color-inherit">
       <Icon name="arrow" />
     </ButtonBack>
     <span className="cc-gallery-lightbox__nav-item cc-gallery-lightbox__nav-item--count">
       {`${currentSlide} / ${slideCount}`}
     </span>
-    <ButtonNext className="cc-gallery-lightbox__nav-item cc-gallery-lightbox__nav-item--next">
+    <ButtonNext className="cc-gallery-lightbox__nav-item cc-gallery-lightbox__nav-item--next u-color-inherit">
       <Icon name="arrow" />
     </ButtonNext>
   </div>
 );
 
-export const GalleryLightBox = ({ slides }: GalleryLightBoxProps) => (
-  <dialog className="cc-gallery-lightbox">
-    <CarouselProvider
-      className="cc-gallery-lightbox__carousel"
-      dragEnabled={false}
-      naturalSlideWidth={16}
-      naturalSlideHeight={9}
-      totalSlides={slides.length}
-    >
-      <Slider
-        className="cc-gallery-lightbox__slider"
-        classNameTray="cc-gallery-lightbox__carousel-tray"
-        classNameTrayWrap="cc-gallery-lightbox__carousel-tray-wrap"
+export const GalleryLightBox = ({ slides, isOpen }: GalleryLightBoxProps) => {
+  /**
+   * Because we want to set the initial info pane state differently dependent
+   * on the browser width.
+   *
+   * #6673 TODO: Add useEffect hook to handle browser width changing,
+   * therefore requiring us to set the infoPane to visible by default (probably
+   * provding the user hasn't already touched it...)
+   */
+  const { isMobile } = useContext(ViewportContext);
+  const initialInfoPaneVisibility = !isMobile;
+
+  const [isInfoPaneVisible, setIsInfoPaneVisible] = useState(
+    initialInfoPaneVisibility
+  );
+
+  const toggleInfoPane = () => {
+    setIsInfoPaneVisible(!isInfoPaneVisible);
+  };
+
+  const infoPaneClassNames = {
+    pane: cx('cc-gallery-lightbox__info-pane', {
+      'cc-gallery-lightbox__info-pane--hidden': !isInfoPaneVisible
+    }),
+    toggle: cx('cc-gallery-lightbox__slide-actions-toggle', {
+      'cc-gallery-lightbox__slide-actions-toggle--is-active': isInfoPaneVisible
+    })
+  };
+
+  return (
+    <dialog className="cc-gallery-lightbox" open={isOpen}>
+      <CarouselProvider
+        className="cc-gallery-lightbox__carousel"
+        dragEnabled={false}
+        naturalSlideWidth={16}
+        naturalSlideHeight={9}
+        totalSlides={slides.length}
       >
-        {slides.map((slide: GalleryLightBoxSlideProps, index: number) => (
-          <Slide
-            className="cc-gallery-lightbox__slide"
-            index={index}
-            innerClassName="cc-gallery-lightbox__slide-layout"
-            innerTag="figure"
-            key={`gallery-lightbox-slide-${slide.src}`}
-          >
-            <div className="cc-gallery-lightbox__image-pane">
-              <div className="cc-gallery-lightbox__image-pane-stage">
-                <ImageElement alt={slide.alt} src={slide.src} />
-              </div>
-              <span className="cc-gallery-lightbox__image-pane-tray">
-                <GalleryLightBoxNav
-                  slideCount={slides.length}
-                  currentSlide={index + 1}
-                />
-                {!!(slide.credit || slide.license) && (
-                  <dl className="cc-gallery-lightbox__meta">
-                    {slide.credit && (
-                      <>
-                        <dt className="cc-gallery-lightbox__meta-item cc-gallery-lightbox__meta-item--label">
-                          Image credit:
-                        </dt>
-                        <dd
-                          className="cc-gallery-lightbox__meta-item cc-gallery-lightbox__meta-item--text"
-                          dangerouslySetInnerHTML={{ __html: slide.credit }}
-                        />
-                      </>
-                    )}
-                    {slide.license && (
-                      <>
-                        <dt className="cc-gallery-lightbox__meta-item cc-gallery-lightbox__meta-item--label">
-                          Image license:
-                        </dt>
-                        <dd
-                          className="cc-gallery-lightbox__meta-item cc-gallery-lightbox__meta-item--text"
-                          dangerouslySetInnerHTML={{ __html: slide.license }}
-                        />
-                      </>
-                    )}
-                  </dl>
-                )}
-                <span className="cc-gallery-lightbox__download">
-                  <a
-                    href={slide.src}
-                    download
-                    className="cc-gallery-lightbox__download-link"
-                  >
-                    <span className="cc-gallery-lightbox__download-icon">
-                      <Icon name="download" />
+        <Slider
+          className="cc-gallery-lightbox__slider"
+          classNameTray="cc-gallery-lightbox__slider-tray"
+          classNameTrayWrap="cc-gallery-lightbox__slider-tray-wrap"
+        >
+          {slides.map((slide: GalleryLightBoxSlideProps, index: number) => (
+            <Slide
+              className="cc-gallery-lightbox__slide"
+              index={index}
+              innerClassName="cc-gallery-lightbox__slide-layout"
+              innerTag="figure"
+              key={`gallery-lightbox-slide-${slide.src}`}
+            >
+              <div className="cc-gallery-lightbox__image-pane">
+                <div className="cc-gallery-lightbox__image-pane-stage">
+                  <ImageElement alt={slide.alt} src={slide.src} />
+                </div>
+                <span className="cc-gallery-lightbox__image-pane-tray">
+                  <div className="cc-gallery-lightbox__slide-actions">
+                    <GalleryLightBoxNav
+                      slideCount={slides.length}
+                      currentSlide={index + 1}
+                    />
+                    <Button
+                      className={infoPaneClassNames.toggle}
+                      icon="chevronThin"
+                      iconPlacementSwitch
+                      onClick={toggleInfoPane}
+                      variant="unstyled"
+                    >
+                      {isInfoPaneVisible ? `Hide` : `Show`} info
+                    </Button>
+                  </div>
+                  {!!(slide.credit || slide.license) && (
+                    <dl className="cc-gallery-lightbox__meta">
+                      {slide.credit && (
+                        <span className="cc-gallery-lightbox__meta-item">
+                          <dt className="cc-gallery-lightbox__meta-item-label">
+                            Image credit:{' '}
+                          </dt>
+                          <dd
+                            className="cc-gallery-lightbox__meta-item-text"
+                            dangerouslySetInnerHTML={{ __html: slide.credit }}
+                          />
+                        </span>
+                      )}
+                      {slide.license && (
+                        <span className="cc-gallery-lightbox__meta-item">
+                          <dt className="cc-gallery-lightbox__meta-item-label">
+                            Image license:{' '}
+                          </dt>
+                          <dd
+                            className="cc-gallery-lightbox__meta-item-text"
+                            dangerouslySetInnerHTML={{ __html: slide.license }}
+                          />
+                        </span>
+                      )}
+                    </dl>
+                  )}
+                  <span className="cc-gallery-lightbox__download">
+                    <a
+                      href={slide.src}
+                      download
+                      className="cc-gallery-lightbox__download-link u-color-inherit"
+                    >
+                      <span className="cc-gallery-lightbox__download-icon">
+                        <Icon name="download" />
+                      </span>
+                      Download
+                    </a>
+                    <span className="cc-gallery-lightbox__download-filesize">
+                      {`[${(slide.fileSize / (1024 * 1024)).toFixed(2)} MB]`}
                     </span>
-                    Download
-                  </a>
-                  <span className="cc-gallery-lightbox__download-filesize">
-                    {`[${(slide.fileSize / (1024 * 1024)).toFixed(2)} MB]`}
                   </span>
                 </span>
-              </span>
-            </div>
-            <figcaption
-              className="cc-gallery-lightbox__info-pane"
-              dangerouslySetInnerHTML={{ __html: slide.caption }}
-            />
-          </Slide>
-        ))}
-      </Slider>
-    </CarouselProvider>
-  </dialog>
-);
+              </div>
+              <figcaption
+                className={infoPaneClassNames.pane}
+                dangerouslySetInnerHTML={{ __html: slide.caption }}
+              />
+            </Slide>
+          ))}
+        </Slider>
+      </CarouselProvider>
+    </dialog>
+  );
+};
 
 /**
  * TODO #6673: Integrate GalleryLighBox
