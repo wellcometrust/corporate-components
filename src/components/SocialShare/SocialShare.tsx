c351d0a5b5c1b1b715011fd5967225dce94e3d8e
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-07-06 15:22:20 +0100

    #6671 feat: Added social share to gallery lightbox

--- src/components/SocialShare/SocialShare.tsx
+++ src/components/SocialShare/SocialShare.tsx
@@ -4,6 +4,8 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 import Button from 'Button';
 import Icon from 'Icon';
 
+import extractTextContent from 'utils/extract-text-content';
+
 type SocialShareProps = {
   body: string;
   hasCopyLink?: boolean;
@@ -19,6 +21,13 @@ export const SocialShare = ({
 }: SocialShareProps) => {
   const [copied, setCopied] = useState(false);
 
+  const bodyText = extractTextContent(body);
+
+  const handleShareLink = () => {
+    // TODO analytics event
+    setCopied(true);
+  };
+
   // TODO: introduce data array or map for dynamically generating social links
   return (
     <div className="cc-social-share">
@@ -30,18 +39,18 @@ export const SocialShare = ({
             target="_blank"
             rel="noopener noreferrer"
           >
-            <span className="u-visually-hidden">Facebook</span>
+            <span className="u-visually-hidden">Share on Facebook</span>
             <Icon name="facebook" className="cc-social-share__icon" />
           </a>
         </li>
         <li className="cc-social-share__item">
           <a
-            href={`https://twitter.com/intent/tweet?text=${title}%20%40wellcometrust%20&amp;url=${url}?utm_source=twitterShare`}
+            href={`https://twitter.com/intent/tweet?text=${bodyText}%20%40wellcometrust%20&amp;url=${url}?utm_source=twitterShare`}
             className="cc-social-share__link"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <span className="u-visually-hidden">Twitter</span>
+            <span className="u-visually-hidden">Tweet</span>
             <Icon name="twitter" className="cc-social-share__icon" />
           </a>
         </li>
@@ -52,13 +61,13 @@ export const SocialShare = ({
             target="_blank"
             rel="noopener noreferrer"
           >
-            <span className="u-visually-hidden">LinkedIn</span>
+            <span className="u-visually-hidden">Share on LinkedIn</span>
             <Icon name="linkedin" className="cc-social-share__icon" />
           </a>
         </li>
         <li className="cc-social-share__item">
           <a
-            href={`mailto:?subject=Shared%20from%20Wellcome%3A%20${title}&amp;body=${body}: ${url}?utm_source=emailShare %0A%0A---%0A%0ASign up to the Wellcome newsletter: https://wellcome.ac.uk/newsletters/subscribe-to-wellcome-newsletter-temp`}
+            href={`mailto:?subject=Shared%20from%20Wellcome%3A%20${title}&amp;body=${bodyText}: ${url}?utm_source=emailShare %0A%0A---%0A%0ASign up to the Wellcome newsletter: https://wellcome.ac.uk/newsletters/subscribe-to-wellcome-newsletter-temp`}
             className="cc-social-share__link"
             target="_blank"
             rel="noopener noreferrer"
@@ -69,16 +78,18 @@ export const SocialShare = ({
         </li>
         {hasCopyLink && (
           <li className="cc-social-share__item">
-            <CopyToClipboard text={url} onCopy={() => setCopied(true)}>
+            <CopyToClipboard text={url} onCopy={handleShareLink}>
               <Button className="cc-social-share__link" variant="unstyled">
-                <span className="u-visually-hidden">Share</span>
+                <span className="u-visually-hidden">Share a link</span>
                 <Icon name="shareLink" className="cc-social-share__icon" />
               </Button>
             </CopyToClipboard>
+            {copied && (
+              <span className="cc-social-share__copied">URL copied</span>
+            )}
           </li>
         )}
       </ul>
-      {copied && <span className="cc-social-share__copied">URL copied</span>}
     </div>
   );
 };
