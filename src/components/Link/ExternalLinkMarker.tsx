3d4d16d419d487dc6ccbd810f747d7599a5e1fa3
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-09-11 14:44:54 +0100

    #7256 bug: External marker icon - corrected styled attrs

--- src/components/Link/ExternalLinkMarker.tsx
+++ src/components/Link/ExternalLinkMarker.tsx
@@ -6,7 +6,7 @@ const ExternalLinkIndicator = () => (
   <>
     <span className="u-visually-hidden">(opens in a new tab)</span>
     <span className="u-external-link-indicator">
-      <Icon name="externalLink" height="0.8125em" width="0.8125em" />
+      <Icon name="externalLink" height="0.85em" width="0.825em" />
     </span>
   </>
 );
