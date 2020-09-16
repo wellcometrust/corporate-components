c143ac690261eed3cb0761ffcc9dca71db27b3f3
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-09-16 14:34:51 +0100

    #7137 feat(style): Timeline initial styles

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -27,9 +27,12 @@ export const Timeline = ({ className, milestones, title }: TimelineProps) => {
       {title && <h3 className="cc-timeline__title">{title}</h3>}
       <dl className="cc-timeline__list">
         {milestones.map((item, index) => (
-          // index used to ensure no two <dt><dd> combos have identical keys
-          // eslint-disable-next-line react/no-array-index-key
-          <Fragment key={`${index}-${item.date}-${item.title}`}>
+          <div
+            // index used to ensure no two <dt><dd> combos have identical keys
+            // eslint-disable-next-line react/no-array-index-key
+            key={`${index}-${item.date}-${item.title}`}
+            className="cc-timeline__item"
+          >
             <dt className="cc-timeline__item-date">{item.date}</dt>
             <dd className="cc-timeline__item-details">
               <h4 className="cc-timeline__item-title">{item.title}</h4>
@@ -40,7 +43,7 @@ export const Timeline = ({ className, milestones, title }: TimelineProps) => {
                 </a>
               )}
             </dd>
-          </Fragment>
+          </div>
         ))}
       </dl>
     </div>
