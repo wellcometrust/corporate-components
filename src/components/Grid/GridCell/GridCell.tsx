967647a2a23118ee1d16cae33b44def496864747
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2020-10-06 14:54:23 +0100

    7431 feat: add nav wrapper to pagination component

--- src/components/Grid/GridCell/GridCell.tsx
+++ src/components/Grid/GridCell/GridCell.tsx
@@ -3,17 +3,20 @@ import cx from 'classnames';
 
 type GridCellProps = {
   children: React.ReactNode;
+  className?: string;
   column: 1 | 2 | 3;
   columnCount: 1 | 2 | 3;
 };
 
 export const GridCell = ({
   children,
+  className,
   column,
   columnCount = 3
 }: GridCellProps) => {
   const classNames = cx('grid__cell', {
-    [`grid__cell--${column}-of-${columnCount}`]: column && columnCount
+    [`grid__cell--${column}-of-${columnCount}`]: column && columnCount,
+    [`${className}`]: className
   });
 
   return <div className={classNames}>{children}</div>;
