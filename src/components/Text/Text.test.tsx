7599584606b83455518554c4d830b8880497fdb7
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2020-10-21 17:57:06 +0100

    #7521 feat: Rich text external link icons - updated regex to filter out email addresses and remove unnecessary capturing groups, added external icon specific tests

--- src/components/Text/Text.test.tsx
+++ src/components/Text/Text.test.tsx
@@ -8,6 +8,12 @@ describe('<Text />', () => {
   const output = shallow(
     <Text className="My text">{`
       <h3>Heading 3</h3>
+      <p><a href="/">Internal link</a></p>
+      <p><a target="_blank" href="mailto:email@wellcome.org">Email address link</a></p>
+      <p><a href="https://wellcome.org" target="_blank">Absolute Wellcome link</a></p>
+      <p><a href="https://www.wellcome.ac.uk" target="_blank">Absolute Wellcome.ac.uk link</a></p>
+      <p><a href="http://test.com" target="_blank">External link</a></p>
+      <p><a href="https://who.org" target="_blank">Another external link</a></p>
       <p><script>alert('xss')</script>Paragraph hiding a script tag</p>
       <script>alert('another xss')</script>
     `}</Text>
@@ -33,5 +39,14 @@ describe('<Text />', () => {
         .dive()
         .find('script')
     ).toHaveLength(0);
+  });
+
+  it('ignores email links and absolute links to wellcome.org or wellcome.ac.uk when adding external link icons', () => {
+    expect(
+      output
+        .find(RichText)
+        .dive()
+        .find('.u-link-new-window')
+    ).toHaveLength(2);
   });
 });
