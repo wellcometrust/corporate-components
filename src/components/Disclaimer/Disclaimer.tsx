3e920ee2865d449d621d08c4ef6934c067a71097
chriddell <chrisliddell1991@gmail.com>
2020-10-30 12:51:28 +0000

    #7579 feat: updates Disclaimer component to use updated props

--- src/components/Disclaimer/Disclaimer.tsx
+++ src/components/Disclaimer/Disclaimer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cx from 'classnames';
 
 import Grid, { GridCell } from 'Grid';
@@ -7,56 +7,68 @@ import Section from 'Section';
 import Button from 'Button';
 
 type DisclaimerProps = {
-  title?: string;
   body?: string;
+  buttons: {
+    href?: string;
+    text?: string;
+    type: 'agree' | 'cancel' | 'link';
+    variant: 'link' | 'primary' | 'secondary' | 'ghost' | 'unstyled';
+  }[];
   className?: string;
-  buttons: [];
+  onAccept: () => void;
+  title?: string;
 };
 
-const titleDefault = 'Disclaimer title';
-const bodyDefault = 'Body goes here';
-const classDefault = 'cc-info-box';
+const defaultButtonText = {
+  agree: 'Agree',
+  cancel: 'Cancel',
+  link: 'Back'
+};
 
 export const Disclaimer = ({
-  title = titleDefault,
-  body = bodyDefault,
-  className = classDefault,
-  buttons = []
+  body,
+  buttons = [],
+  className,
+  onAccept,
+  title
 }: DisclaimerProps) => {
+  const [wasAccepted, setWasAccepted] = useState(false);
+
+  const classNames = cx('cc-info-box cc-disclaimer', {
+    [className]: className
+  });
+
+  const handleAgree = () => {
+    if (typeof onAccept === 'function') onAccept();
+    setWasAccepted(true);
+  };
+
   return (
-    <Section className={className}>
-      <Grid>
-        <GridCell column={1} columnCount={1}>
-          <h3>{title}</h3>
-          <RichText>{body}</RichText>
-          <div className="cookie-message__buttons">
-            {buttons &&
-              buttons.map((button: any) => {
-                const defaultText: { [key: string]: any } = {
-                  agree: 'Agree',
-                  cancel: 'Cancel',
-                  link: 'Back'
-                };
-
-                const buttonType = button.attributes.button_type;
-                const linkStyle = button.attributes.link_style;
-                const { link } = button.attributes;
-
-                return (
+    !wasAccepted && (
+      <Section className={classNames}>
+        <Grid>
+          <GridCell column={1} columnCount={1}>
+            <h3>{title}</h3>
+            <RichText>{body}</RichText>
+            <div className="cookie-message__buttons">
+              {buttons &&
+                buttons.map(({ text, type, variant, href }) => (
                   <Button
+                    className="cc-disclaimer__button"
+                    href={href.length ? href : null}
+                    key={`disclaimer-button-${type}`}
+                    onClick={type === 'agree' && handleAgree}
                     type="button"
-                    className="disclaimer__button"
-                    variant={linkStyle}
-                    href={link?.uri}
+                    variant={variant}
                   >
-                    {link?.title || defaultText[buttonType]}
+                    {text || defaultButtonText[type]}
                   </Button>
-                );
-              })}
-          </div>
-        </GridCell>
-      </Grid>
-    </Section>
+                ))}
+            </div>
+          </GridCell>
+        </Grid>
+      </Section>
+    )
   );
 };
 
