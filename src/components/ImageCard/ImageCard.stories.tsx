71bbdea3537cc6edd7131ca5f556745c8b0eaf72
chriddell <chrisliddell1991@gmail.com>
2020-11-18 17:26:41 +0000

    #7749 feat: adds ImageCardMultiple storybook story

--- src/components/ImageCard/ImageCard.stories.tsx
+++ src/components/ImageCard/ImageCard.stories.tsx
@@ -4,7 +4,7 @@ import { select, text } from '@storybook/addon-knobs';
 
 import ImageCard from './ImageCard';
 
-const ImageCardExample = () => {
+const SingleImageCard = () => {
   const authors = text(
     'authors',
     'Christiane Hertz-Fowler, Georgia Walton, Chonnettia Jones'
@@ -39,6 +39,71 @@ const ImageCardExample = () => {
   );
 };
 
-const stories = storiesOf('Components|ImageCard', module);
+const MultipleImageCard = () => {
+  const cardCount = select('How many ImageCards?', [2, 3], 3, 'General');
 
-stories.add('ImageCard', ImageCardExample);
+  const knobs = [...Array(cardCount).keys()].map(i => ({
+    authors: text(
+      'authors',
+      'Christiane Hertz-Fowler, Georgia Walton, Chonnettia Jones',
+      `Card ${i + 1}`
+    ),
+    date: text('Date', '03/08/1991', `Card ${i + 1}`),
+    imageAlt: text('Image alt text', 'Alternative image text', `Card ${i + 1}`),
+    imageSrc: text(
+      'Image path',
+      `https://via.placeholder.com/300`,
+      `Card ${i + 1}`
+    ),
+    title: text('Title', 'My section', `Card ${i + 1}`),
+    titleAs: select(
+      'Title element',
+      ['h2', 'h3', 'h4', 'h5', 'h6'],
+      'h3',
+      `Card ${i + 1}`
+    ),
+    topic: text('Topic', 'Climate Change', `Card ${i + 1}`),
+    url: text('URL', '/news/all', `Card ${i + 1}`)
+  }));
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'nowrap',
+        marginLeft: 'calc(-1 * var(--space-xl))'
+      }}
+    >
+      {[...Array(cardCount).keys()].map(i => (
+        <div
+          style={{
+            flexBasis: '33.33%',
+            flexGrow: 1,
+            marginBottom: 'var(--space-unit)',
+            marginLeft: 'var(--space-xl)'
+          }}
+        >
+          <ImageCard
+            authors={knobs[i].authors
+              .trim()
+              .split(',')
+              .map(a => a.trim())}
+            date={knobs[i].date}
+            imageAlt={knobs[i].imageAlt}
+            imageSrc={knobs[i].imageSrc}
+            title={knobs[i].title}
+            titleAs={knobs[i].titleAs}
+            topic={knobs[i].topic}
+            url={knobs[i].url}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const stories = storiesOf('ImageCard', module);
+
+stories.add('Single', SingleImageCard);
+stories.add('Multiple', MultipleImageCard);
