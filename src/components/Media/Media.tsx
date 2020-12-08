4faff57e712e6cab269920b778176b94799ffadc
chriddell <chrisliddell1991@gmail.com>
2020-12-08 12:44:13 +0000

    #7793 feat(style): use _media.scss to set grid for component

--- src/components/Media/Media.tsx
+++ src/components/Media/Media.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import cx from 'classnames';
 
 import MediaCaption from './MediaCaption';
-import Grid, { GridCell } from '../Grid';
 
 type MediaProps = {
   caption?: string;
@@ -27,23 +26,15 @@ const Media = ({
 
   return (
     <figure className={classNames}>
-      <Grid>
-        <GridCell column={2} columnCount={3}>
-          <div className={`cc-media__element cc-media__element--${type}`}>
-            {children}
-          </div>
-        </GridCell>
-        <GridCell column={3} columnCount={3}>
-          <div className="cc-media__element">
-            <MediaCaption
-              caption={caption}
-              className="cc-media--wide__caption"
-              credit={credit}
-              licence={licence}
-            />
-          </div>
-        </GridCell>
-      </Grid>
+      <div className={`cc-media__element cc-media__element--${type}`}>
+        {children}
+      </div>
+      <MediaCaption
+        caption={caption}
+        className="cc-media--wide__caption"
+        credit={credit}
+        licence={licence}
+      />
     </figure>
   );
 };
