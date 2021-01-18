9bf401cb130e69ca77a22ec08568db847c10a319
chriddell <chrisliddell1991@gmail.com>
2021-01-18 17:28:07 +0000

    #7938 feature: adds status label to Timeline, updates Timeline test and storybook components

--- src/components/Timeline/Timeline.stories.tsx
+++ src/components/Timeline/Timeline.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text } from '@storybook/addon-knobs';
+import { date, text } from '@storybook/addon-knobs';
+import { formatISO } from 'date-fns';
 
 import Timeline from './Timeline';
 import Readme from './Timeline.md';
@@ -12,11 +13,17 @@ const TimelineExample = () => {
   const milestone3ID = 'Milestone 3';
 
   const title = text('title', 'Beatles Albums', generalID);
+  const statusLabel = text(
+    'status label',
+    'Open to new submissions',
+    generalID
+  );
 
   const milestones = [
     {
       body: text('Milestone 1 body', 'This is the body text', milestone1ID),
-      date: text('Milestone 1 date', '1964', milestone1ID),
+      date: formatISO(date('Milestone 1 date', new Date(), milestone1ID)),
+      dateLabel: text('Milestone 1 date label', '1964', milestone1ID),
       linkHref: text(
         'Milestone 1 link href',
         '/beatles-for-sale',
@@ -27,33 +34,44 @@ const TimelineExample = () => {
         'Read more about Beatles for Sale',
         milestone1ID
       ),
+      statusLabel: text('Milestone 1 status label', null, milestone1ID),
       title: text('Milestone 1 title', 'Beatles for Sale', milestone1ID)
     },
     {
       body: text('Milestone 2 body', 'This is the body text', milestone2ID),
-      date: text('Milestone 2 date', '1965', milestone2ID),
+      date: formatISO(date('Milestone 2 date', new Date(), milestone2ID)),
+      dateLabel: text('Milestone 2 date', '1965', milestone2ID),
       linkHref: text('Milestone 2 link href', '/help', milestone2ID),
       linkText: text(
         'Milestone 2 link text',
         'Read more about Help',
         milestone2ID
       ),
+      statusLabel: text('Milestone 2 status label', null, milestone2ID),
       title: text('Milestone 2 title', 'Help', milestone2ID)
     },
     {
       body: text('Milestone 3 body', 'This is the body text', milestone3ID),
-      date: text('Milestone 3 date', '1965', milestone3ID),
+      date: formatISO(date('Milestone 3 date', new Date(), milestone3ID)),
+      dateLabel: text('Milestone 3 date', '1965', milestone3ID),
       linkHref: text('Milestone 3 link href', '/rubber-soul', milestone3ID),
       linkText: text(
         'Milestone 3 link text',
         'Read more about Rubber Soul',
         milestone3ID
       ),
+      statusLabel: text('Milestone 3 status label', null, milestone3ID),
       title: text('Milestone 3 title', 'Rubber Soul', milestone3ID)
     }
   ];
 
-  return <Timeline milestones={milestones} title={title} />;
+  return (
+    <Timeline
+      initialStatusLabel={statusLabel}
+      milestones={milestones}
+      title={title}
+    />
+  );
 };
 
 const stories = storiesOf('Components|Timeline', module);
--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
+import { isPast, parseISO } from 'date-fns';
 
 import RichText from 'RichText';
 
 type TimelineMilestoneProps = {
   body?: string;
   date: string;
+  dateLabel: string;
   linkHref?: string;
   linkText?: string;
+  statusLabel?: string;
   title: string;
 };
 
 type TimelineProps = {
   className?: string;
   description?: string;
+  initialStatusLabel?: string;
   milestones: TimelineMilestoneProps[];
   title?: string;
 };
@@ -21,12 +25,61 @@ type TimelineProps = {
 export const Timeline = ({
   className,
   description,
+  initialStatusLabel,
   milestones,
   title
 }: TimelineProps) => {
   const classNames = cx('cc-timeline', {
     [className]: className
   });
+  const [statusLabel, setStatusLabel] = useState(initialStatusLabel);
+
+  /**
+   * In order to display the most up-to-date information in the browser
+   * we must filter our array of `milestones` to find the:
+   *
+   * - most recent milestone (milestone.date)
+   * - which has a statusLabel (milestone.statusLabel)
+   *
+   * @returns {string} statusLabel
+   */
+  const getCurrentStatusLabel = () => {
+    const currentMilestone = milestones
+      .filter(
+        milestone =>
+          milestone.statusLabel &&
+          // check the date value is a valid Date
+          typeof parseISO(milestone.date).getTime === 'function'
+      )
+      // convert date string to Date object (for Array.sort)
+      .map(milestone => ({
+        ...milestone,
+        date: parseISO(milestone.date)
+      }))
+      // use getTime b/c Typescript
+      .sort((a, b) => a.date.getTime() - b.date.getTime())
+      /**
+       * Returns first item in array which has a date in the past
+       * with optional chaining to return statusLabel if present.
+       */
+      .find(milestone => isPast(milestone.date));
+    return currentMilestone?.statusLabel ?? null;
+  };
+
+  /**
+   * We must run getCurrentStatusLabel() on the client (rather than
+   * passing the relevant data at build time) because the value is
+   * date/time-sensitive.
+   *
+   * This method should avoid discrepencies in the value when the
+   * component is statically rendered.
+   */
+  useEffect(() => {
+    setStatusLabel(prevStatusLabel => {
+      const newStatusLabel = getCurrentStatusLabel();
+      return newStatusLabel || initialStatusLabel;
+    });
+  }, [milestones]);
 
   return (
     <div className={classNames}>
@@ -34,31 +87,34 @@ export const Timeline = ({
       {description && (
         <RichText className="cc-timeline__description">{description}</RichText>
       )}
-      <ol className="cc-timeline__list">
-        {milestones.map((item, index) => (
-          <li
-            // index used to ensure no two <dt><dd> combos have identical keys
-            // eslint-disable-next-line react/no-array-index-key
-            key={`${index}-${item.date}-${item.title}`}
-            className="cc-timeline__item"
-          >
-            <div className="cc-timeline__item-date">{item.date}</div>
-            {item.linkHref && item.linkText && (
-              <a className="cc-timeline__item-link" href={item.linkHref}>
-                {item.linkText}
-              </a>
-            )}
-            <div className="cc-timeline__item-details">
-              <h4 className="cc-timeline__item-title">{item.title}</h4>
-              {item.body && (
-                <RichText className="cc-timeline__item-body">
-                  {item.body}
-                </RichText>
+      <div className="cc-timeline__main">
+        {statusLabel && <p className="cc-timeline__status">{statusLabel}</p>}
+        <ol className="cc-timeline__list">
+          {milestones.map((item, index) => (
+            <li
+              // index used to ensure no two <dt><dd> combos have identical keys
+              // eslint-disable-next-line react/no-array-index-key
+              key={`${index}-${item.date}-${item.title}`}
+              className="cc-timeline__item"
+            >
+              <div className="cc-timeline__item-date">{item.date}</div>
+              {item.linkHref && item.linkText && (
+                <a className="cc-timeline__item-link" href={item.linkHref}>
+                  {item.linkText}
+                </a>
               )}
-            </div>
-          </li>
-        ))}
-      </ol>
+              <div className="cc-timeline__item-details">
+                <h4 className="cc-timeline__item-title">{item.title}</h4>
+                {item.body && (
+                  <RichText className="cc-timeline__item-body">
+                    {item.body}
+                  </RichText>
+                )}
+              </div>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
