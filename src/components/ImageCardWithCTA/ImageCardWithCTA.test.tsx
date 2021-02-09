75a5748e83d8b4bf6555289ef5d961bd735b6541
drs2034 <d.shrestha34@gmail.com>
2021-02-09 17:13:40 +0000

    Add a test script for Image card with cta.

--- src/components/ImageCardWithCTA/ImageCardWithCTA.test.tsx
+++ src/components/ImageCardWithCTA/ImageCardWithCTA.test.tsx
@@ -1,19 +1,22 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import ImageCardWithCTA from './ImageCardWithCTA';
+import ImageCardWithCTA from './ImageCardWithCTA';
 
-// describe('<ImageCardWithCTA />', () => {
-//   const output = shallow(
-//     <ImageCardWithCTA
-//       description="The true impact of the COVID-19 pandemic will be felt beyond its immediate effects. Jeremy Farrar explains why the choices leaders make now will help define the 21st century."
-//       href="/news/our-response-covid-19-will-help-define-21st-century"
-//       title="Our response to COVID-19 will help define the 21st century"
-//       linkText="Optional text link"
-//     />
-//   );
+describe('<ImageCardWithCTA />', () => {
+  const output = shallow(
+    <ImageCardWithCTA
+      description="The true impact of the COVID-19 pandemic will be felt beyond its immediate effects. Jeremy Farrar explains why the choices leaders make now will help define the 21st century."
+      href="/news/our-response-covid-19-will-help-define-21st-century"
+      imageAlt="Image alt tag"
+      imageSrc="https://via.placeholder.com/300"
+      linkText="Optional text link"
+      title="Our response to COVID-19 will help define the 21st century"
+      titleAs="h3"
+    />
+  );
 
-//   it('renders the component', () => {
-//     expect(output);
-//   });
-// });
+  it('renders the component', () => {
+    expect(output);
+  });
+});
