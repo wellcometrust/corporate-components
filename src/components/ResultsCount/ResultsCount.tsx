d29d514d59d1b30ef8e349f1a51dc8817d4bc5a7
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2021-02-15 14:34:19 +0000

    #8120: feat add select to ResultsCount

--- src/components/ResultsCount/ResultsCount.tsx
+++ src/components/ResultsCount/ResultsCount.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import cx from 'classnames';
+import Label from '../Label';
 
 type ResultsCountProps = {
   className?: string;
   currentCount: string;
+  id?: string;
+  onChange?: () => void;
+  options?: {
+    label: string;
+    value: string;
+  }[];
   resultsCount: number;
-  sortedBy: string;
+  selectedValue?: string;
+  sortedBy?: string;
 };
 
 export const ResultsCount = ({
   className,
   currentCount,
+  id,
+  onChange,
+  options,
   resultsCount,
+  selectedValue,
   sortedBy
 }: ResultsCountProps) => {
   const classNames = cx('cc-results-count', {
@@ -26,7 +38,30 @@ export const ResultsCount = ({
           {currentCount} results of {resultsCount}
         </strong>
       </p>
-      <p className="cc-results-count__sort">Sorted by {sortedBy}</p>
+      {sortedBy && (
+        <p className="cc-results-count__sort">Sorted by {sortedBy}</p>
+      )}
+      {options && (
+        <div className="cc-results-count__sort">
+          <Label
+            className="cc-results-count__label"
+            htmlFor={id}
+            text="Sort by:"
+          />
+          <select
+            className="cc-select cc-results-count__select"
+            id={id}
+            onChange={onChange}
+            value={selectedValue}
+          >
+            {options?.map(option => (
+              <option key={`option-${option.label}`} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
     </div>
   );
 };
