875403dcbb0a0eda54343411af10fc418af9af2f
chriddell <chrisliddell1991@gmail.com>
2021-02-26 15:48:06 +0000

    #7930 task: adds key prop to QuickLinks map return items

--- src/components/QuickLinks/QuickLinks.tsx
+++ src/components/QuickLinks/QuickLinks.tsx
@@ -15,10 +15,11 @@ export const QuickLinks = ({ className, items }: QuickLinksProps) => {
   const classNames = cx('c-quick-links', {
     className
   });
+
   return (
     <ul className={classNames}>
-      {items.map(({ href, label }) => (
-        <li className="c-quick-links__item">
+      {items.map(({ href, label }, i) => (
+        <li className="c-quick-links__item" key={`quick-links-item-${i + 1}`}>
           <Link className="c-quick-links__item-link" to={href}>
             {label}
           </Link>
