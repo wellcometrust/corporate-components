08bed71d6391714e4aabb2a84acf145453cafc9a
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2021-03-10 18:00:14 +0000

    8293 task(style): update filters

--- src/components/SidebarFilter/SidebarFilter.tsx
+++ src/components/SidebarFilter/SidebarFilter.tsx
@@ -4,7 +4,6 @@ import cx from 'classnames';
 import Button from 'Button';
 import Accordion, { AccordionItem } from 'Accordion/Accordion';
 import Checkbox from 'Checkbox';
-import Icon from 'Icon';
 
 type SidebarFilterProps = {
   activeTags: {
@@ -66,7 +65,7 @@ export const SidebarFilter = ({
           {activeTags.map(({ label, value }) => (
             <Button
               className="cc-sidebar-filter__tags-list-item"
-              icon="close"
+              icon="closeSmall"
               iconPlacementSwitch
               key={value}
               onClick={() => onTagRemove(value)}
@@ -87,6 +86,7 @@ export const SidebarFilter = ({
                   <li className="cc-sidebar-filter__list-item" key={item.value}>
                     <Checkbox
                       checked={item.isActive}
+                      className="cc-sidebar-filter__checkbox"
                       disabled={item.isDisabled}
                       id={item.label}
                       label={item.label}
