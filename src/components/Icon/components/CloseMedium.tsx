b9237ab3f71788915f5e7623d8eae61c97379d1c
Updated composer dependencies <C.Reid@wellcome.ac.uk>
2021-03-16 11:32:14 +0000

    8287 task(chore): Site header close icon - converted SVG attribute to JSX

--- src/components/Icon/components/CloseMedium.tsx
+++ src/components/Icon/components/CloseMedium.tsx
@@ -7,7 +7,7 @@ const CloseMedium = (props: IconSVGProps) => (
     <path
       d="M.167.167a.57.57 0 01.805 0L5 4.194 9.028.167a.57.57 0 11.805.805L5.806 5l4.027 4.028a.57.57 0 01.066.726l-.066.08a.57.57 0 01-.805 0L5 5.805.972 9.833a.57.57 0 11-.805-.805L4.194 5 .167.972A.57.57 0 01.1.246z"
       fill="currentColor"
-      fill-rule="evenodd"
+      fillRule="evenodd"
     />
   </svg>
 );
