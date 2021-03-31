148f11b6f83ace2312eec65c95f0ad2568efb996
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2021-03-31 14:58:01 +0100

    add setup

--- config/setupTests.js
+++ config/setupTests.js
@@ -2,5 +2,7 @@ import { configure } from 'enzyme/build';
 import Adapter from 'enzyme-adapter-react-16/build';
 import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
 
+require('@testing-library/jest-dom/extend-expect');
+
 configure({ adapter: new Adapter() });
 registerRequireContextHook();
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React, { ComponentProps } from 'react';
+import { screen, render } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import Button from './Button';
 
-describe('<Button />', () => {
-  const output = shallow(<Button>Button text</Button>);
+const setup = (props: Partial<ComponentProps<typeof Button>> = {}) => {
+  const defaultProps: ComponentProps<typeof Button> = {
+    children: 'Some button',
+    ...props
+  };
 
-  it('renders the component', () => {
-    expect(output);
+  return render(<Button {...defaultProps} />);
+};
+
+describe('<Button />', () => {
+  it('renders button', () => {
+    const { container } = setup({});
+    expect(container).toMatchSnapshot();
+  });
+  it('allow onClick handler', () => {
+    const onClick = jest.fn();
+    setup({ onClick });
+
+    const btn = screen.getByRole('button', { name: /Some button/ });
+
+    expect(onClick).not.toHaveBeenCalled();
+    user.click(btn);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+  it('can specify href and therefore change base component from button to anchor', () => {
+    setup({ href: 'http://google.com' });
+
+    const btn = screen.getByRole('link', { name: /Some button/ });
+    expect(btn).toHaveAttribute('href', 'http://google.com');
   });
 });
