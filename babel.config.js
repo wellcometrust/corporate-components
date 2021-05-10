c6c544ab02fc0f2caefae2b2c0c6d90aa1a3e40b
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2021-05-10 11:36:26 +0100

    task: changes after npm audit fix --force

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
 
   const presets = [
-    "@babel/preset-env",
-    "@babel/preset-react",
-    "@babel/preset-typescript"
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript'
   ];
 
   const plugins = [
@@ -12,13 +12,20 @@ module.exports = function (api) {
     [
       '@babel/plugin-transform-runtime',
       {
-        'corejs': 3
+        corejs: 3
       }
-    ]
+    ],
+    /**
+     * Add these options to silence the Storybook build warnings about different loose mode configuration
+     * @see https://github.com/babel/babel/issues/11622
+     */
+    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+    ['@babel/plugin-proposal-private-methods', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
   ];
 
   return {
     plugins,
-    presets,
-  }
+    presets
+  };
 };
