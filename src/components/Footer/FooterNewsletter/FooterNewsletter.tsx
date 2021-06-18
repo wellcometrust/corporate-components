6769640f71d6ae2ab417785b521e924ddc42be38
Ewelina Skibinska <E.Skibinska@wellcome.ac.uk>
2021-06-18 13:56:59 +0100

    4557 bug: fixes screen reader not able to read newsletter email name

--- src/components/Footer/FooterNewsletter/FooterNewsletter.tsx
+++ src/components/Footer/FooterNewsletter/FooterNewsletter.tsx
@@ -4,7 +4,10 @@ import NewsletterForm from 'NewsletterForm';
 
 export const FooterNewsletter = () => (
   <div className="footer-newsletter">
-    <NewsletterForm className="footer-newsletter__form">
+    <NewsletterForm
+      className="footer-newsletter__form"
+      emailInputId="footer-newsletter"
+    >
       <p className="footer-newsletter__intro">
         Get the latest news about Wellcome and the work we fund in a monthly
         email.
